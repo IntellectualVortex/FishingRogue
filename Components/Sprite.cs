e2519eaf1479a7ddd5d937fa12d287b38f0e77bd
Dominic Senarsky <dominico.sebastian@gmail.com>
2024-02-02 21:11:49 +0100

    removed useless usings

--- Components/Sprite.cs
+++ Components/Sprite.cs
@@ -1,14 +1,8 @@
 ﻿#region Includes
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using System;
-using System.Reflection;
-using System.Reflection.Emit;
-using System.Xml.Linq;
+
 #endregion
 
 namespace FishingRogue
--- Worlds/World1.cs
+++ Worlds/World1.cs
@@ -1,15 +1,6 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System;
+﻿
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Dynamic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+
 
 namespace FishingRogue
 {
